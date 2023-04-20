f31ad0b242d44b331903bf84dd304bf94d2130e3
adazd <zefzef>
2023-04-20 02:30:05 +0200

    Destruction de la scène en trop

---
+++ gameapk_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,201 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000004 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000005 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000006 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000007 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4 (void);
+// 0x00000008 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3 (void);
+// 0x00000011 System.Int32 Batiment::get_Prix()
+extern void Batiment_get_Prix_m2642A61F1CDD6F13B10B7A48A775F867570809B6 (void);
+// 0x00000012 System.Void Batiment::Start()
+extern void Batiment_Start_m2EB9BB6003CD48FFCFA77C839453914DCA002216 (void);
+// 0x00000013 System.Void Batiment::Update()
+extern void Batiment_Update_m3E8B03102DD2348299C042D361CE2E9C8780ABE9 (void);
+// 0x00000014 System.Void Batiment::Ameliorer()
+extern void Batiment_Ameliorer_m2C46015460B31928F4048F5D09FCF82121B7818C (void);
+// 0x00000015 System.Void Batiment::.ctor()
+extern void Batiment__ctor_m48E16C54E5A2E5852B927041D14679158482C80E (void);
+// 0x00000016 System.Void Echoppe::Start()
+extern void Echoppe_Start_m12E785678D4A3BA69E9BEC45F30FB943EE3679E7 (void);
+// 0x00000017 System.Void Echoppe::Update()
+extern void Echoppe_Update_m03FF17F4393EC5E09C720392B8CD8B8CE9AD75DC (void);
+// 0x00000018 System.Void Echoppe::.ctor()
+extern void Echoppe__ctor_m1940F3AF671273D4AA2DCC1AA507C7FFBDC05A5A (void);
+// 0x00000019 System.Void Magasin::Start()
+extern void Magasin_Start_mDE1A5187A70FE3D972652CBED9B18393E19D5F96 (void);
+// 0x0000001A System.Void Magasin::Update()
+extern void Magasin_Update_m6B5728C06FD65C057DC82D03A27A41E71EACCB94 (void);
+// 0x0000001B System.Void Magasin::.ctor()
+extern void Magasin__ctor_mB74B23D78822C8C4785931108EA4BA9BE75E0A03 (void);
+// 0x0000001C GameManager GameManager::get_Instance()
+extern void GameManager_get_Instance_m076FE4D98E785B5AEE0B4C360C7857F824E7FBD0 (void);
+// 0x0000001D System.Void GameManager::Start()
+extern void GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2 (void);
+// 0x0000001E System.Void GameManager::Update()
+extern void GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41 (void);
+// 0x0000001F System.Void GameManager::UpdateTextArgent()
+extern void GameManager_UpdateTextArgent_mD60811821590DE02F0CE269F28A66B0106A99A63 (void);
+// 0x00000020 System.Boolean GameManager::UpdateMonnaie(System.Int32)
+extern void GameManager_UpdateMonnaie_mB536D94D206E45AEB45E852D4CF6E51F8637B876 (void);
+// 0x00000021 System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x00000022 System.Void SpawnableManager::Start()
+extern void SpawnableManager_Start_mFC8BFB2C5F789510DC84AD28B8BF57D00291CDED (void);
+// 0x00000023 System.Void SpawnableManager::Update()
+extern void SpawnableManager_Update_m7A9A38650830610C5C81A77D22ABB3A011E23220 (void);
+// 0x00000024 System.Void SpawnableManager::SpawnPrefab(UnityEngine.Vector3)
+extern void SpawnableManager_SpawnPrefab_mF522EF88B8AAB43C88ADC75967DDEE81030F05DB (void);
+// 0x00000025 System.Void SpawnableManager::ModeCreation()
+extern void SpawnableManager_ModeCreation_mE20F8CECBBE3E97115F1FA1EBE917913EF3BEEE3 (void);
+// 0x00000026 System.Void SpawnableManager::ModeDelation()
+extern void SpawnableManager_ModeDelation_m9F2BB693BFAA851909E96BFB1801B64E7FCACB6E (void);
+// 0x00000027 System.Void SpawnableManager::ModeAmelioration()
+extern void SpawnableManager_ModeAmelioration_m1FF51E6593605EF3D13FED261DBEBAB4E23FD6F2 (void);
+// 0x00000028 System.Void SpawnableManager::.ctor()
+extern void SpawnableManager__ctor_m5E989BEEF4E8A6A12828E117976ED1043ACAA1ED (void);
+static Il2CppMethodPointer s_methodPointers[40] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3,
+	Batiment_get_Prix_m2642A61F1CDD6F13B10B7A48A775F867570809B6,
+	Batiment_Start_m2EB9BB6003CD48FFCFA77C839453914DCA002216,
+	Batiment_Update_m3E8B03102DD2348299C042D361CE2E9C8780ABE9,
+	Batiment_Ameliorer_m2C46015460B31928F4048F5D09FCF82121B7818C,
+	Batiment__ctor_m48E16C54E5A2E5852B927041D14679158482C80E,
+	Echoppe_Start_m12E785678D4A3BA69E9BEC45F30FB943EE3679E7,
+	Echoppe_Update_m03FF17F4393EC5E09C720392B8CD8B8CE9AD75DC,
+	Echoppe__ctor_m1940F3AF671273D4AA2DCC1AA507C7FFBDC05A5A,
+	Magasin_Start_mDE1A5187A70FE3D972652CBED9B18393E19D5F96,
+	Magasin_Update_m6B5728C06FD65C057DC82D03A27A41E71EACCB94,
+	Magasin__ctor_mB74B23D78822C8C4785931108EA4BA9BE75E0A03,
+	GameManager_get_Instance_m076FE4D98E785B5AEE0B4C360C7857F824E7FBD0,
+	GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2,
+	GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41,
+	GameManager_UpdateTextArgent_mD60811821590DE02F0CE269F28A66B0106A99A63,
+	GameManager_UpdateMonnaie_mB536D94D206E45AEB45E852D4CF6E51F8637B876,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	SpawnableManager_Start_mFC8BFB2C5F789510DC84AD28B8BF57D00291CDED,
+	SpawnableManager_Update_m7A9A38650830610C5C81A77D22ABB3A011E23220,
+	SpawnableManager_SpawnPrefab_mF522EF88B8AAB43C88ADC75967DDEE81030F05DB,
+	SpawnableManager_ModeCreation_mE20F8CECBBE3E97115F1FA1EBE917913EF3BEEE3,
+	SpawnableManager_ModeDelation_m9F2BB693BFAA851909E96BFB1801B64E7FCACB6E,
+	SpawnableManager_ModeAmelioration_m1FF51E6593605EF3D13FED261DBEBAB4E23FD6F2,
+	SpawnableManager__ctor_m5E989BEEF4E8A6A12828E117976ED1043ACAA1ED,
+};
+static const int32_t s_InvokerIndices[40] = 
+{
+	5411,
+	4384,
+	5517,
+	5517,
+	5517,
+	5517,
+	8673,
+	5453,
+	4418,
+	5517,
+	5517,
+	5517,
+	4270,
+	4384,
+	5517,
+	8673,
+	5383,
+	5517,
+	5517,
+	5517,
+	5517,
+	5517,
+	5517,
+	5517,
+	5517,
+	5517,
+	5517,
+	8634,
+	5517,
+	5517,
+	5517,
+	2999,
+	5517,
+	5517,
+	5517,
+	4468,
+	5517,
+	5517,
+	5517,
+	5517,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	40,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
